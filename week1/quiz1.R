ebce606758524951a8141de19e6b66ed7c649fc0
Paul Sarmiento <paulsarmient14@gmail.com>
2014-12-09 01:43:22 +0800

    first commit

---
+++ week1/quiz1.R
@@ -0,0 +1,30 @@
+setwd("/Users/Paul/Desktop/R/Getting and Cleaning Data/")
+
+data <- read.table( "getdata-data-ss06hid.csv", 
+                    sep = ",", 
+                    nrows = 6497,
+                    header = TRUE )
+
+## Answer#1: Read property more than 1M
+table(data[,c("VAL")] >= 24 )
+
+## Read data of FES
+table(data[,c("FES")])
+
+## Answer#3: Exported from excel file to CSV
+## having some problems with xlsx package
+data_3 <- read.csv("hw1_data_3.csv")
+sum(data_3$Zip * data_3$Ext, na.rm=T) 
+
+## Answer#4: XMLs
+library(XML)
+fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- xmlTreeParse(fileUrl,useInternal=TRUE)
+rootNode <- xmlRoot(doc)
+
+table(xpathSApply(rootNode,"//zipcode",xmlValue))["21231"]
+
+## Answer#5: DT
+file_5 = "getdata-data-ss06pid.csv"
+library(data.table)
+DT <- fread(file_5)
---
+++ week2/quiz2_week2.R
@@ -0,0 +1,49 @@
+setwd("/Users/Paul/Desktop/R/Getting and Cleaning Data/")
+
+#From: https://github.com/hadley/httr/blob/master/demo/oauth2-github.r
+library(httr)
+GITHUB_CONSUMER_SECRET <- "97b235e82834c69cbf323938cc66ec5008fdc2c6"
+oauth_endpoints("github")
+
+myapp <- oauth_app("github", "2ebf5f3f6462bff2e469", secret = GITHUB_CONSUMER_SECRET)
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+
+gtoken <- config(token = github_token)
+req <- GET("https://api.github.com/users/jtleek/repos", gtoken)
+stop_for_status(req)
+response_json <- content(req, as = "text")
+
+library(jsonlite)
+
+#put response req into a data frame using jsonlite
+doc <- fromJSON(response_json)
+ans <- doc[doc$name == "datasharing",c(2,45)]
+print(ans) #answer to 1
+
+#----------- number 2 ----------------
+library(sqldf)
+acs <- read.csv("getdata-data-ss06pid.csv")
+sqldf("select agep,pwgtp1 from acs where AGEP < 50")
+
+#----------- number 4 ----------------
+con = url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode = readLines(con)
+close(con)
+html_lines <- c(10,20,30,100)
+nchar(htmlCode[html_lines]) #answer to 4
+
+#----------- number 5 ----------------
+n_offsets <- c(-1,9,-5,rep(c(4,4,-5),3),4,4)
+colnames = c("Week", 
+             "Nino1+2 SST", "Nino1+2 SSTA",
+             "Nino3 SST", "Nino3 SSTA",
+             "Nino34 SST", "Nino34 SSTA",
+             "Nino4 SST", "Nino4 SSTA")
+df_5 <- read.fwf("getdata-wksst8110.for",
+                 widths = n_offsets,
+                 skip = 4,
+                 col.names = colnames)
+
+ans5 <- sum(df_5[,4])
+print(ans5)
+
